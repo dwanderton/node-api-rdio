43db223fd355725eb702d40d6dde8b94142e7af5
David Anderton <dwanderton@gmail.com>
2017-10-04 23:05:39 -0400

    add logging

--- server.js
+++ server.js
@@ -1,8 +1,67 @@
 var express = require('express');
 var app = express();
 
+/* logging start */
+const winston = require('winston');
+var expressWinston = require('express-winston');
+const fs = require('fs');
+const env = process.env.NODE_ENV || 'development';
+const logDir = 'log';
+// Create the log directory if it does not exist
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+const tsFormat = () => (new Date()).toLocaleTimeString();
+const logger = new (winston.Logger)({
+  transports: [
+    // colorize the output to the console
+    new (winston.transports.Console)({
+      timestamp: tsFormat,
+      colorize: true,
+      level: 'info'
+    }),
+    new (require('winston-daily-rotate-file'))({
+      filename: `${logDir}/-results.log`,
+      timestamp: tsFormat,
+      maxsize: 5242880,
+      datePattern: 'yyyy-MM-dd',
+      prepend: true,
+      level: env === 'development' ? 'verbose' : 'info'
+    })
+  ]
+});
+
+app.use(expressWinston.logger({
+  transports: [
+    new winston.transports.Console({
+      //json: true,
+      colorize: true
+    })
+  ],
+  colorize: true,
+  meta: false,
+  msg: "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+}));
+app.use(expressWinston.errorLogger({
+  transports: [
+    new winston.transports.Console({
+      json: true,
+      colorize: true
+    })
+  ]
+}));
+
+/* logger.debug('Debugging info');
+logger.verbose('Verbose info');
+logger.info('Hello world');
+logger.warn('Warning message');
+logger.error('Error info');
+
+logging end */
+
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
+
 var port = process.env.PORT || 8080;
 
 // set the view engine to ejs
@@ -19,5 +78,5 @@ app.get('/', function(req, res) {
 });
 
 app.listen(port, function() {
-	console.log('Our app is running on http://localhost:' + port);
+  logger.info('app is running on http://localhost:' + port);
 });
